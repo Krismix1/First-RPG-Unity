45c2259772c362aa8e6dbaa8864c682d6c74e8e5
Epharas <cbetivu@gmail.com>
2017-10-09 21:50:26 +0200

    Add character stats as armour, damage and health and modifiers for them

---
+++ First-RPG/Assets/Scripts/Stats/CharacterStats.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+
+public class CharacterStats : MonoBehaviour {
+
+    public Stat damage;
+    public Stat armour;
+
+    public int maxHealth = 100;
+    public int CurrentHealth { get; private set; }
+
+    // Use this for initialization
+    void Awake() {
+        CurrentHealth = maxHealth;
+    }
+
+    private void Update() {
+        // For testing taking of damage and armour reduction
+        if (Input.GetKeyDown(KeyCode.T)) {
+            TakeDamage(10);
+        }
+    }
+
+    public void TakeDamage(int damage) {
+        damage -= armour.GetValue();
+        damage = Mathf.Clamp(damage, 0, int.MaxValue);
+
+        CurrentHealth -= damage;
+        Debug.Log(transform.name + " takes " + damage + " damage");
+
+        if (CurrentHealth <= 0) {
+            Die();
+        }
+    }
+
+    public virtual void Die() {
+        Debug.Log(transform.name + " died.");
+    }
+}
---
+++ First-RPG/Assets/Scripts/Stats/PlayerStats.cs
@@ -0,0 +1,18 @@
+﻿public class PlayerStats : CharacterStats {
+
+    private void Start() {
+        EquipmentManager.instance.onEquipmentChanged += OnEquipmentChanged;
+    }
+
+    private void OnEquipmentChanged(Equipment newItem, Equipment oldItem) {
+        if(newItem != null) {
+            armour.AddModifier(newItem.armourModifier);
+            damage.AddModifier(newItem.damageModifier);
+        }
+
+        if(oldItem != null) {
+            armour.RemoveModifier(oldItem.armourModifier);
+            damage.RemoveModifier(oldItem.damageModifier);
+        }
+    }
+}
